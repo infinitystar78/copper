1fa301d6d679f1c26497650c2ccbda10088eac9b
Melvin Wijenayaka <melvin.wijenayaka@copper.co>
2021-08-17 17:19:26 +0100

    TOOLS:Project setup

---
+++ Copper Platform QA/Copper_Platform_QAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Copper_Platform_QAApp.swift
+//  Copper Platform QA
+//
+//  Created by Melvin Wijenayaka on 17/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Copper_Platform_QAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
